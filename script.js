4ff751d5a6255bf5352c6833776585874197545d
antePandzic <120681630+antePandzic@users.noreply.github.com>
2023-07-13 09:09:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let currentImageIndex = 0;
     const images = document.querySelectorAll('.slider-image');
-    const nextButton = document.getElementById('next');
-    const prevButton = document.getElementById('prev');
-
+    const nextImageButton = document.getElementById('next-image');
+    const prevImageButton = document.getElementById('prev-image');
+  
     images[currentImageIndex].classList.add('active');
-
-    nextButton.addEventListener('click', () => {
-        images[currentImageIndex].classList.remove('active');
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        images[currentImageIndex].classList.add('active');
+  
+    nextImageButton.addEventListener('click', () => {
+      images[currentImageIndex].classList.remove('active');
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      images[currentImageIndex].classList.add('active');
     });
-
-    prevButton.addEventListener('click', () => {
-        images[currentImageIndex].classList.remove('active');
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        images[currentImageIndex].classList.add('active');
+  
+    prevImageButton.addEventListener('click', () => {
+      images[currentImageIndex].classList.remove('active');
+      currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+      images[currentImageIndex].classList.add('active');
+    });
+  
+    let currentCardIndex = 0;
+    const cards = document.querySelectorAll('.card-container .card');
+    const nextCardButton = document.getElementById('next-card');
+  
+    cards[currentCardIndex].classList.add('active');
+  
+    nextCardButton.addEventListener('click', () => {
+      cards[currentCardIndex].classList.remove('active');
+      currentCardIndex = (currentCardIndex + 1) % cards.length;
+      cards[currentCardIndex].classList.add('active');
     });
 });
